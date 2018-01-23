2b007525fd0e85e6a3369125057519cf98eee56a
nascVi <vaiiho.nascimbene@gmail.com>
2018-01-23 23:59:19 +0100

    cloudinary cv

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -9,7 +9,7 @@ class NavMenu extends Component {
         <StyledLink to={'/pride'}>Pride</StyledLink>
         <StyledLink to={'/knowhow'}>Know How</StyledLink>
         <StyledLink to={'/whoiam'}>Who I Am</StyledLink>
-        <StyledA href={'https://res.cloudinary.com/provideup/image/upload/f_pdf/v1/provider/yum9u4l9ohwbfx9jbob4'}>Resume</StyledA>
+        <StyledA href={'https://docs.google.com/viewer?url=https%3A%2F%2Fres.cloudinary.com%2Fprovideup%2Fimage%2Fupload%2Ff_pdf%2Fv1%2Fprovider%2Ftkus29irgsgpxr3elsni.pdf'}>Resume</StyledA>
       </Container>
     );
   }
